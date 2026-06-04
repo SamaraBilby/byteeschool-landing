031d76f81ab492197bed4bf28b52e9136f4b7351
Samara Bilby <98292955+SamaraBilby@users.noreply.github.com>
2026-06-04 13:28:24 -0300

    feat: add localized route page

---
+++ app/pages/[locale].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { isSupportedLocale } from "~/utils/i18n";
+
+const route = useRoute();
+const localeParam = Array.isArray(route.params.locale) ? route.params.locale[0] : route.params.locale;
+
+if (!isSupportedLocale(localeParam)) {
+  throw createError({ statusCode: 404, statusMessage: "Page not found" });
+}
+</script>
+
+<template>
+  <LandingPage />
+</template>
